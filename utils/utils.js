c07df06624838f83d79df1cc5b60d761f33d32e9
dayasudhan <dayasudhankg@gmail.com>
2023-10-27 20:08:21 +0530

    build complete

--- utils/utils.js
+++ utils/utils.js
@@ -1,2 +1,2 @@
-//export default BASE_URL = "http://13.232.42.12:3006/";
-export default BASE_URL = "http://192.168.91.124:3006/";
+export default BASE_URL = "http://13.232.42.12:3006/";
+//export default BASE_URL = "http://192.168.91.124:3006/";
