65daeb4c97771ac9605a57ca0d7dd0ba4f98fae6
dayasudhan <dayasudhankg@gmail.com>
2023-11-08 09:58:35 +0530

    Drawerscrenn

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler'; // Required for gesture-based navigation
+import React  from 'react';
+import { NavigationContainer, } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { MaterialCommunityIcons,Entypo  } from '@expo/vector-icons';
 import card from './screens/card.js'
@@ -8,8 +9,29 @@ import enquiries from './screens/Enquiries.js'
 import itemDetail from './screens/itemDetail.js'
 import EnquiryInput from './screens/EnquiryInput.js'
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { View, Image, StyleSheet,Text,Button ,TouchableOpacity } from 'react-native';
+import { useNavigationState } from '@react-navigation/native';
+const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
+
+const MainStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={card} />
+      <Stack.Screen name="ItemDetail" component={itemDetail} /> 
+      <Stack.Screen name="Enquiries" component={EnquiryInput} /> 
+    </Stack.Navigator>
+  );
+}
+const SellerStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Sell" component={seller} />
+    </Stack.Navigator>
+  );
+}
 
 function MyTabs() {
   return (
@@ -22,9 +44,9 @@ function MyTabs() {
     >
       <Tab.Screen
         name="Home"
-        component={card}
+        component={MainStackNavigator}
         options={{
-          tabBarLabel: 'Home',
+          //tabBarLabel: 'Home',
           tabBarIcon: ({ color }) => (
             <Entypo name="shop" color={color} size={26} />
           ),
@@ -32,7 +54,7 @@ function MyTabs() {
       />
       <Tab.Screen
         name="Sell"
-        component={seller}
+        component={SellerStackNavigator}
         options={{
           tabBarLabel: 'Sell',
           tabBarIcon: ({ color }) => (
@@ -40,7 +62,7 @@ function MyTabs() {
           ),
         }}
       />
-      <Tab.Screen
+      {/* <Tab.Screen
          name="Enquiries"
         component={enquiries}
         options={{
@@ -50,20 +72,137 @@ function MyTabs() {
           ),
         }}
         
-      />
+      /> */}
 
     </Tab.Navigator>
   );
 }
 
+
+const CustomDrawerContent = () => {
+  return (
+    <View style={styles.drawerHeader}>
+      <Image
+        source={require('./assets/ic_launcher.png')}
+        style={styles.drawerImage}
+      />
+    </View>
+  );
+};
+
+function DrawerContent({ navigation }) {
+  const navigationState = useNavigationState((state) => state);
+  
+  const navigateToScreen = (screenName) => {
+    navigation.navigate(screenName);
+  };
+
+  const isScreenActive = (routeName) => {
+    if (navigationState) {
+      return navigationState.routes[navigationState.index].name === routeName;
+    }
+    return false;
+  };
+  return (
+    <View>
+      <Text>Farmify</Text>
+      <Image
+          source={require('./assets/ic_launcher.png')}
+          style={styles.drawerImage}
+        />
+
+       <TouchableOpacity
+        style={[
+          styles.drawerItem,
+          isScreenActive('Home') && styles.activeDrawerItem,
+        ]}
+        onPress={() => navigateToScreen('Home')}
+      >
+        <MaterialCommunityIcons name="home" size={30} color="black" />
+        <Text style={styles.drawerText}>Home</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[
+          styles.drawerItem,
+          isScreenActive('Enquiries') && styles.activeDrawerItem,
+        ]}
+        onPress={() => navigateToScreen('Enquiries')}
+      >
+        <MaterialCommunityIcons name="inbox" size={30} color="black" />
+        <Text style={styles.drawerText}>Enquiries</Text>
+      </TouchableOpacity>
+    </View>
+    
+  );
+
+}
+const DrawerNavigator = () => {
+  return (
+      <Drawer.Navigator 
+      drawerContent={(props) => <DrawerContent {...props} />}>
+      <Drawer.Screen
+        name="Home12"
+        component={MyTabs}
+        options={{
+          title: 'Farmify',
+          drawerIcon: ({ focused, size }) => (
+            <MaterialCommunityIcons
+              name={focused ? 'home' : 'home-outline'}
+              size={size}
+              color={focused ? 'blue' : 'black'}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Enquiries"
+        component={enquiries}
+        options={{
+          title: 'Enquiries',
+          drawerIcon: ({ focused, size }) => (
+            <MaterialCommunityIcons
+              name={focused ? 'inbox' : 'inbox-outline'}
+              size={size}
+              color={focused ? 'blue' : 'black'}
+            />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
+const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+  },
+  drawerHeader: {
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  drawerImage: {
+    height: 120,
+    width: 120,
+    borderRadius: 60,
+  },
+  drawerItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  drawerText: {
+    marginLeft: 20,
+    fontSize: 18,
+  },
+  activeDrawerItem: {
+    backgroundColor: 'lightblue',
+  },
+});
 export default function App() {
   return (
      <NavigationContainer>
-     <Stack.Navigator initialRouteName="Tabs">
-       <Stack.Screen name="Farmify" component={MyTabs} />
-       <Stack.Screen name="ItemDetail" component={itemDetail} /> 
-       <Stack.Screen name="Enquiries" component={EnquiryInput} /> 
-     </Stack.Navigator>
+     <DrawerNavigator/>
    </NavigationContainer>
   );
 }
