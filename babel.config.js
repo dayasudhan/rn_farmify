44e6774bf3f2a22c8d5aed5ecda7c20be0389fcc
dayasudhan <dayasudhankg@gmail.com>
2024-07-05 10:27:44 +0530

    upgrading the expo version

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    'react-native-reanimated/plugin',
     ['@babel/plugin-transform-class-properties', { loose: true }],
     ['@babel/plugin-transform-private-methods', { loose: true }],
     ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    //['react-native-reanimated/plugin'],
+    ['module-resolver', {
+      root: ['./src'],
+      alias: {
+        '@components': './src/components',
+        '@assets': './src/assets',
+        '@screens': './src/screens',
+        // Add other aliases as needed
+      },
+    }],
   ],
 };
